7f0a188a92434d5d6706f1e19d4d9e5de3d79eb4
RKamey <ralonso@ucol.mx>
2024-03-11 00:41:58 -0600

    ✨ Renderizado condicional basado en el estado del usuario en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-// import { useState } from 'react'
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+
+// Import firebase modules
+import appFirebase from '../src/credentials';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+const auth = getAuth(appFirebase);
+
+// Import components
+import Login from '../src/components/Login'
+import Home from '../src/components/Home'
 
 import './App.css'
 
 function App() {
 
+  const [user, setUser] = useState(null);
+
+  onAuthStateChanged(auth, (firebaseUser) => {
+    if (firebaseUser) {
+      setUser(firebaseUser)
+    } else {
+      setUser(null)
+    }
+  })
+
   return (
     <div>
-      <h1>App</h1>
+      {user ? <Home userEmail = {user.email} /> : <Login />}
     </div>
   )
 }
