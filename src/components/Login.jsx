b53645329b64e22ad0c4fae89d0640f7fcd428d7
RKamey <ralonso@ucol.mx>
2024-03-11 01:25:53 -0600

    ✨ Funcionalidad para registrarse o iniciar sesión (mensaje y botón)

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,16 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import Imagen from '../assets/loginvector.jpg'
 import ImageProfile from '../assets/profile.png'
 
+import appFirebase from '../credentials'
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+const auth = getAuth(appFirebase);
+
 export const Login = () => {
+
+    const [registrando, setRegistrando] = useState(false)
+
   return (
     <div className='container'>
         <div className='row'>
@@ -15,8 +22,9 @@ export const Login = () => {
                         <form>
                             <input type="text" placeholder='Ingresar Email' className='input-style'/>
                             <input type="password" placeholder='Ingresar contraseña' className='input-style' />
-                            <button className='btn-form'>Registrarse</button>
+                            <button className='btn-form'>{ registrando ? "Regístrate" : "Inicia Sesión" }</button>
                         </form>
+                        <h4 className='text'>{registrando ? "Si ya tienes cuenta" : "No tienes cuenta"}<button className='btn-switch' onClick={()=>setRegistrando(!registrando)}>{registrando ? "Inicia sesíon" : "Regístrate"}</button> </h4>
                     </div>
                 </div>
             </div>
